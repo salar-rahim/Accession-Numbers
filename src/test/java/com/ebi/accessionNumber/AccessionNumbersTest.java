ca87d69df7f3480609edec3694197a4230f3953a
salar <salar.rahim@ymail.com>
2017-07-20 12:34:58 +0300

    Add class AccessionNumbersTest Add method testAdd()

---
+++ src/test/java/com/ebi/accessionNumber/AccessionNumbersTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ebi.accessionNumber;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Salar
+ */
+public class AccessionNumbersTest extends TestCase {
+    
+    public AccessionNumbersTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of add method, of class AccessionNumbers.
+     */
+    public void testAdd() {
+        System.out.println("add");
+        String key = "";
+        String value = "";
+        AccessionNumbers.add(key, value);
+        
+    }
+    
+}
