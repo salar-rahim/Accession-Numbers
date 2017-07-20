dedaf2a2f7b32499f0624654ecbded81d0ea1294
salar <salar.rahim@ymail.com>
2017-07-20 22:11:53 +0300

    Call methods from main class

--- src/main/java/com/ebi/accessionNumber/App.java
+++ src/main/java/com/ebi/accessionNumber/App.java
@@ -10,8 +10,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        String [] numbers = {"A00000","A0001","ERR000111","ERR000112","ERR000113"};
+        String [] numbers = {"A00000","A0001","ERR000111","ERR000112","ERR000113","ERR000115","ERR000116",
+            "ERR100114","ERR200000001","ERR200000002","ERR200000003","DRR2110012","SRR211001","ABCDEFG1"};
         Arrays.stream(numbers).sorted().forEach(AccessionNumbers::add);
+        AccessionNumbers.buildNumberRanges();
+        System.out.println(RangeBuilder.getOrderedNumberRanges());
     
         
     }
