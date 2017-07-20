66bac98956081d64670d31d0aec1b7122c974a1b
salar <salar.rahim@ymail.com>
2017-07-20 05:16:28 +0300

    Add class AccessionNumbers

---
+++ src/main/java/com/ebi/accessionNumber/AccessionNumbers.java
@@ -0,0 +1,15 @@
+package com.ebi.accessionNumber;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Salar
+ */
+public class AccessionNumbers {
+    private static Map<String,List<String>> numbers = new HashMap<>();
+    
+
+}
