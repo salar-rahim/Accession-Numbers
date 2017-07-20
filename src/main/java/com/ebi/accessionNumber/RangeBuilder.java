d683512369e11555a33ddc39b75d096e71e8cf6b
salar <salar.rahim@ymail.com>
2017-07-20 18:31:37 +0300

    Add method compareLength(String,String) Add method isGreaterByOne(int x, int y)

--- src/main/java/com/ebi/accessionNumber/RangeBuilder.java
+++ src/main/java/com/ebi/accessionNumber/RangeBuilder.java
@@ -1,9 +1,23 @@
 package com.ebi.accessionNumber;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Salar
  */
 public class RangeBuilder {
+    static List<String> accNumbersList = new ArrayList<>();
+    
+    private static boolean compareLength(String str1, String str2){
+        return (str1.length()==str2.length())? true:false;
+    }
+    
+    private static boolean isGreaterByOne(int x, int y){
+        return (x+1 == y)? true : false;
+    }
+    
+    
 
 }
