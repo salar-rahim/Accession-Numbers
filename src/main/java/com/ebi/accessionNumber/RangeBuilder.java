51d1c82b6e07d25157e5e9cb1bbe6e0f15ab2776
salar <salar.rahim@ymail.com>
2017-07-20 21:04:06 +0300

    Add method buildNumberRanges(Entry) Add method findRangeEnd(List,int)

--- src/main/java/com/ebi/accessionNumber/RangeBuilder.java
+++ src/main/java/com/ebi/accessionNumber/RangeBuilder.java
@@ -2,6 +2,7 @@ package com.ebi.accessionNumber;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
 
 /**
  *
@@ -21,6 +22,41 @@ public class RangeBuilder {
     static void buildNumberRanges(){
         AccessionNumbers.alphanumeric.entrySet().parallelStream()
                                                 .forEach(RangeBuilder::buildNumberRanges);
+    }
+    
+    private static void buildNumberRanges(Entry entry){
+        String key = (String) entry.getKey();
+        List<String> values = (ArrayList<String>) entry.getValue();
+        if (values.size() > 1) {
+            String rangeStart,rangeEnd;
+            for (int i = 0; i < values.size()-1; i++) {
+               rangeStart = values.get(i);
+               rangeEnd = values.get(i+1);
+                if (compareLength(rangeStart, rangeEnd) && isGreaterByOne(Integer.parseInt(rangeStart),Integer.parseInt(rangeEnd))) {
+                    rangeEnd = findRangeEnd(values, i);
+                    accNumbersList.add(createRangeString(key, rangeStart, rangeEnd));
+                    i = values.indexOf(rangeEnd);
+                    continue;
+                } else if(i == values.size()-2) {
+                    accNumbersList.add(key.concat(rangeEnd));
+                }
+                accNumbersList.add(key.concat(rangeStart));
+            }
+        } else {
+            accNumbersList.add(key.concat(values.get(0)));
+        }
+    }
+    
+    private static String findRangeEnd(List<String> numbersList, int index){
+        String rangeStart="",rangeEnd;
+        for (int i = index+1; i < numbersList.size()-1; i++) {
+            rangeStart = numbersList.get(i);
+            rangeEnd = numbersList.get(i+1);
+            if (!(compareLength(rangeStart, rangeEnd) && isGreaterByOne(Integer.parseInt(rangeStart),Integer.parseInt(rangeEnd)))) {
+                break;
+            }
+        }
+        return rangeStart;
     }
     
      private static String createRangeString(String letter, String rangeStart, String rangeEnd){
