3444b0f823af1f34a7e028c6af9f2f7496db7b43
salar <salar.rahim@ymail.com>
2017-07-20 12:32:49 +0300

    Add App class (the main class)

---
+++ src/main/java/com/ebi/accessionNumber/App.java
@@ -0,0 +1,16 @@
+package com.ebi.accessionNumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    
+//        Arrays.stream(args).sorted().forEach(k -> AccessionNumbers::add);
+    
+    }
+    
+}
