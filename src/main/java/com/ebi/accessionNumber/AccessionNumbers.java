ca500eaba2edb4ed01137b720cc046222b97e839
salar <salar.rahim@ymail.com>
2017-07-21 10:22:38 +0300

    Add descriptive comments to the methods

--- src/main/java/com/ebi/accessionNumber/AccessionNumbers.java
+++ src/main/java/com/ebi/accessionNumber/AccessionNumbers.java
@@ -1,14 +1,13 @@
 package com.ebi.accessionNumber;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  *
- * @author Salar
+ * @author Salar (salar.rahim@gmail.com)
  */
 public class AccessionNumbers {
      static Map<String,List<String>> alphanumeric = new HashMap<>();
@@ -26,9 +25,13 @@ public class AccessionNumbers {
             numbers = new ArrayList<>();
         }
         numbers.add(value);
-        Collections.sort(numbers);
         alphanumeric.put(key, numbers);
     }
+    
+    /**
+     * This method use parallel stream to read the entryset of the hashmap
+     * and deligates it to the buildNumberRanges() method of RangeBuilder class
+     */
      static void buildNumberRanges(){
         AccessionNumbers.alphanumeric.entrySet().parallelStream()
                                                 .forEach(RangeBuilder::buildNumberRanges);
@@ -39,7 +42,7 @@ public class AccessionNumbers {
      * @param accNumber the original accession number
      * @return a series of digits found in the accNumber
      */
-    public static String extractDigits(String accNumber){
+     static String extractDigits(String accNumber){
         return accNumber.replaceAll("\\D+","");
     }
     
@@ -48,7 +51,7 @@ public class AccessionNumbers {
      * @param accNumber the original accession number
      * @return a series of letters found in the accNumber
      */
-    public static String extractLetters(String accNumber){
+     static String extractLetters(String accNumber){
         return accNumber.replaceAll("[^A-Za-z]+","");
     }
     
