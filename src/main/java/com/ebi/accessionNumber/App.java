5f326ecd17b20e21544858a6f59978a957b230fb
salar <salar.rahim@ymail.com>
2017-07-21 09:09:32 +0300

    input is taken from args using stream() method to output the results

--- src/main/java/com/ebi/accessionNumber/App.java
+++ src/main/java/com/ebi/accessionNumber/App.java
@@ -10,12 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        String [] numbers = {"A00000","A0001","ERR000111","ERR000112","ERR000113","ERR000115","ERR000116",
-            "ERR100114","ERR200000001","ERR200000002","ERR200000003","DRR2110012","SRR211001","ABCDEFG1"};
-        Arrays.stream(numbers).sorted().forEach(AccessionNumbers::add);
+        Arrays.stream(args).sorted().forEach(AccessionNumbers::add);
         AccessionNumbers.buildNumberRanges();
-        System.out.println(RangeBuilder.getOrderedNumberRanges());
-    
+        
+        
+        RangeBuilder.getOrderedNumberRanges().stream()
+                                            .sorted()
+                                            .forEach(System.out::println);
         
     }
     
